60548f1fdbfbb4bf1205b2951649c9a0df402059
Sendiko <sarangtawon897@gmail.com>
2025-10-06 16:00:24 +0700

    feat(core): integrate HomeScreen into App composable

--- composeApp/src/commonMain/kotlin/org/chevalierlab/kashier/core/App.kt
+++ composeApp/src/commonMain/kotlin/org/chevalierlab/kashier/core/App.kt
@@ -2,12 +2,13 @@ package org.chevalierlab.kashier.core
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import org.chevalierlab.kashier.home.presentation.HomeScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-
+        HomeScreen()
     }
 }
