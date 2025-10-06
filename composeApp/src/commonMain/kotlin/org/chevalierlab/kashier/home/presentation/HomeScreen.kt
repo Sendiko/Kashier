ada413386362864f25c036fa9e9feb9badf7bdfa
Sendiko <sarangtawon897@gmail.com>
2025-10-06 15:50:25 +0700

    feat(home-presentation): enhance HomeScreen with animations, FAB, and dynamic state management for items

--- composeApp/src/commonMain/kotlin/org/chevalierlab/kashier/home/presentation/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/chevalierlab/kashier/home/presentation/HomeScreen.kt
@@ -1,40 +1,48 @@
 package org.chevalierlab.kashier.home.presentation
 
+import androidx.compose.animation.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import kashier.composeapp.generated.resources.Res
-import kashier.composeapp.generated.resources.all_item_label
-import kashier.composeapp.generated.resources.app_name
-import kashier.composeapp.generated.resources.selected_item_label
+import kashier.composeapp.generated.resources.*
 import org.chevalierlab.kashier.home.data.DummyDataSource
-import org.chevalierlab.kashier.home.presentation.components.HomeSeparator
-import org.chevalierlab.kashier.home.presentation.components.ItemCard
-import org.chevalierlab.kashier.home.presentation.components.SaveButton
-import org.chevalierlab.kashier.home.presentation.components.SelectedItemChip
-import org.chevalierlab.kashier.home.presentation.components.TotalPriceHeader
+import org.chevalierlab.kashier.home.domain.Item
+import org.chevalierlab.kashier.home.presentation.components.*
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
+
+    var totalPrice by remember { mutableStateOf(0.00) }
+    val selectedItems = remember { mutableStateListOf<Item>() }
+    var showSelectedItem by remember { mutableStateOf(true) }
+    var showAllItem by remember { mutableStateOf(true) }
+
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {
                     Text(text = stringResource(Res.string.app_name))
                 }
+            )
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = { TODO("Add Item.") },
+                containerColor = MaterialTheme.colorScheme.tertiary,
+                text = { Text(text = stringResource(Res.string.add_item_fab_label)) },
+                icon = { Icon(Icons.Filled.Add, contentDescription = stringResource(Res.string.add_item_fab_label)) }
             )
         }
     ) { paddingValues ->
@@ -44,7 +52,7 @@ fun HomeScreen() {
             item {
                 TotalPriceHeader(
                     modifier = Modifier.padding(horizontal = 16.dp),
-                    totalPrice = 1000000.00
+                    totalPrice = totalPrice
                 )
             }
             item {
@@ -52,47 +60,71 @@ fun HomeScreen() {
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 16.dp),
-                    onSave = { TODO("Save data.") }
+                    onSave = { TODO("Save data.") },
+                    enabled = selectedItems.isNotEmpty() && totalPrice > 0.00
                 )
             }
             item {
                 HomeSeparator(
                     modifier = Modifier.padding(start = 16.dp, end = 4.dp),
                     title = stringResource(Res.string.selected_item_label),
-                    visible = false,
-                    onAction = { visible -> }
+                    visible = showSelectedItem,
+                    onAction = { visible ->
+                        showSelectedItem = visible
+                    }
                 )
             }
             item {
-                FlowRow(
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    verticalArrangement = Arrangement.spacedBy(4.dp),
-                    horizontalArrangement = Arrangement.spacedBy(4.dp),
-                    content = {
-                        DummyDataSource().getDatas().take(5).map { item ->
-                            SelectedItemChip(
-                                onRemove = { },
-                                item = item
-                            )
+                AnimatedVisibility(
+                    visible = showSelectedItem,
+                    enter = expandVertically(),
+                    exit = shrinkVertically()
+                ) {
+                    FlowRow(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        verticalArrangement = Arrangement.spacedBy(4.dp),
+                        horizontalArrangement = Arrangement.spacedBy(4.dp),
+                        content = {
+                            selectedItems.map { item ->
+                                SelectedItemChip(
+                                    onRemove = {
+                                        selectedItems.remove(item)
+                                        totalPrice -= item.price
+                                    },
+                                    item = item,
+                                    modifier = Modifier.padding(horizontal = 4.dp)
+                                )
+                            }
                         }
-                    }
-                )
+                    )
+                }
             }
             item {
                 HomeSeparator(
                     modifier = Modifier.padding(start = 16.dp, end = 4.dp),
                     title = stringResource(Res.string.all_item_label),
-                    visible = false,
-                    onAction = { visible -> }
+                    visible = showAllItem,
+                    onAction = { visible ->
+                        showAllItem = visible
+                    }
                 )
             }
             items(DummyDataSource().getDatas()) { item ->
-                ItemCard(
-                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp),
-                    item = item,
-                    onEdit = {  },
-                    onAdd = {  }
-                )
+                AnimatedVisibility(
+                    visible = showAllItem,
+                    enter = fadeIn(),
+                    exit = fadeOut()
+                ) {
+                    ItemCard(
+                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp),
+                        item = item,
+                        onEdit = { },
+                        onAdd = {
+                            selectedItems.add(it)
+                            totalPrice += it.price
+                        }
+                    )
+                }
             }
         }
     }
