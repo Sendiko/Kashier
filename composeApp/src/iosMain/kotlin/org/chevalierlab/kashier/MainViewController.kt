67af049859da485f6af8eca8c6bfbd9a91b541dd
Sendiko <sarangtawon897@gmail.com>
2025-10-06 09:25:54 +0700

    chore: remove platform-specific logic and simplify App structure

--- composeApp/src/iosMain/kotlin/org/chevalierlab/kashier/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/chevalierlab/kashier/MainViewController.kt
@@ -1,5 +1,6 @@
 package org.chevalierlab.kashier
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.chevalierlab.kashier.core.App
 
 fun MainViewController() = ComposeUIViewController { App() }
--- composeApp/src/jvmMain/kotlin/org/chevalierlab/kashier/main.kt
+++ composeApp/src/jvmMain/kotlin/org/chevalierlab/kashier/main.kt
@@ -2,6 +2,7 @@ package org.chevalierlab.kashier
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.chevalierlab.kashier.core.App
 
 fun main() = application {
     Window(
