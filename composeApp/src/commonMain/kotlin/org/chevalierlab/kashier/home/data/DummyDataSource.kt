fb9ac1050485a3ca4df5285b1ca1044fbab2c862
Sendiko <sarangtawon897@gmail.com>
2025-10-06 15:04:09 +0700

    feat(home-data): add DummyDataSource for fetching mock item data

---
+++ composeApp/src/commonMain/kotlin/org/chevalierlab/kashier/home/data/DummyDataSource.kt
@@ -0,0 +1,77 @@
+package org.chevalierlab.kashier.home.data
+
+import org.chevalierlab.kashier.home.domain.Item
+import org.chevalierlab.kashier.home.presentation.components.ItemCard
+
+class DummyDataSource {
+
+    fun getDatas(): List<Item> = listOf(
+        Item(
+            id = 1,
+            userId = 1,
+            name = "Item 1",
+            price = 100000.0
+        ),
+        Item(
+            id = 1,
+            userId = 1,
+            name = "Item 1",
+            price = 100000.0
+        ),
+        Item(
+            id = 1,
+            userId = 1,
+            name = "Item 1",
+            price = 100000.0
+        ),
+        Item(
+            id = 1,
+            userId = 1,
+            name = "Item 1",
+            price = 100000.0
+        ),
+        Item(
+            id = 1,
+            userId = 1,
+            name = "Item 1",
+            price = 100000.0
+        ),
+        Item(
+            id = 1,
+            userId = 1,
+            name = "Item 1",
+            price = 100000.0
+        ),
+        Item(
+            id = 1,
+            userId = 1,
+            name = "Item 1",
+            price = 100000.0
+        ),
+        Item(
+            id = 1,
+            userId = 1,
+            name = "Item 1",
+            price = 100000.0
+        ),
+        Item(
+            id = 1,
+            userId = 1,
+            name = "Item 1",
+            price = 100000.0
+        ),
+        Item(
+            id = 1,
+            userId = 1,
+            name = "Item 1",
+            price = 100000.0
+        ),
+        Item(
+            id = 1,
+            userId = 1,
+            name = "Item 1",
+            price = 100000.0
+        ),
+    )
+
+}
