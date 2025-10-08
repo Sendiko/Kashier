2d344be1a116c8693d7778e743137c5c6d0a8d8e
Sendiko <sarangtawon897@gmail.com>
2025-10-08 20:47:01 +0700

    feat(home-presentation): add HomeViewModel with state management and integrate into HomeScreen

--- composeApp/src/commonMain/kotlin/org/chevalierlab/kashier/core/App.kt
+++ composeApp/src/commonMain/kotlin/org/chevalierlab/kashier/core/App.kt
@@ -2,13 +2,22 @@ package org.chevalierlab.kashier.core
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.chevalierlab.kashier.home.presentation.HomeScreen
+import org.chevalierlab.kashier.home.presentation.HomeViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        HomeScreen()
+        val viewModel = viewModel<HomeViewModel>()
+        val state by viewModel.state.collectAsStateWithLifecycle()
+        HomeScreen(
+            state = state,
+            onEvent = viewModel::onEvent
+        )
     }
 }
