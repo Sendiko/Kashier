fdbed0dbb00d939d27a7fd6fb2a62d0b070de2b7
Sendiko <sarangtawon897@gmail.com>
2025-10-18 10:58:28 +0700

    feat(di): initialize Koin on all platforms and configure `KashierApp` for Android
    - Added `initKoin` calls to `JVM` and `iOS` entry points.
    - Created `KashierApp` class extending `Application` to initialize Koin on Android.
    - Updated AndroidManifest to set `KashierApp` as the application class.

--- composeApp/src/iosMain/kotlin/org/chevalierlab/kashier/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/chevalierlab/kashier/MainViewController.kt
@@ -2,5 +2,10 @@ package org.chevalierlab.kashier
 
 import androidx.compose.ui.window.ComposeUIViewController
 import org.chevalierlab.kashier.core.App
+import org.chevalierlab.kashier.core.di.initKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        initKoin()
+    }
+) { App() }
--- composeApp/src/jvmMain/kotlin/org/chevalierlab/kashier/main.kt
+++ composeApp/src/jvmMain/kotlin/org/chevalierlab/kashier/main.kt
@@ -3,8 +3,10 @@ package org.chevalierlab.kashier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import org.chevalierlab.kashier.core.App
+import org.chevalierlab.kashier.core.di.initKoin
 
 fun main() = application {
+    initKoin()
     Window(
         onCloseRequest = ::exitApplication,
         title = "Kashier",
