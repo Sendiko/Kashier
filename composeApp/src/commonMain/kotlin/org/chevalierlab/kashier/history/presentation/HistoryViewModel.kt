7777dd8e0db649ec2e265f6b098aa4283fd7bbf7
Sendiko <sarangtawon897@gmail.com>
2025-10-18 07:23:19 +0700

    feat(history-presentation): add HistoryScreen, HistoryViewModel, and related components for transaction history
    - Implement `HistoryScreen` with state-based UI and navigation.
    - Add `HistoryViewModel` for grouping and managing history data by date.
    - Introduce `HistoryCard` for displaying transaction details.
    - Utilize string resources and core utilities for localizations and formatting.

---
+++ composeApp/src/commonMain/kotlin/org/chevalierlab/kashier/history/presentation/HistoryViewModel.kt
@@ -0,0 +1,49 @@
+package org.chevalierlab.kashier.history.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.datetime.*
+import org.chevalierlab.kashier.history.data.DummyHistoryDataSource
+import org.chevalierlab.kashier.history.domain.History
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
+
+class HistoryViewModel : ViewModel() {
+
+    private val _histories = DummyHistoryDataSource().getHistories()
+    private val _state = MutableStateFlow(HistoryState(histories = groupHistoryByDate(_histories)))
+    val state = _state.asStateFlow()
+
+    @OptIn(ExperimentalTime::class)
+    private fun groupHistoryByDate(histories: List<History>): Map<String, List<History>> {
+        val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
+        today.minus(today.dayOfWeek.isoDayNumber - 1, DateTimeUnit.DAY)
+        val startOfThisMonth = today.minus(today.day - 1, DateTimeUnit.DAY)
+        val startOfLastMonth = startOfThisMonth.minus(1, DateTimeUnit.MONTH)
+
+        return histories.groupBy { history ->
+            when {
+                history.date == today -> "Hari ini"
+                history.date >= today.minus(1, DateTimeUnit.WEEK) -> "Minggu ini"
+                history.date >= startOfThisMonth -> "Bulan ini"
+                history.date >= startOfLastMonth -> {
+                    startOfLastMonth.month.getDisplayName()
+                }
+
+                else -> {
+                    val monthName = history.date.month.getDisplayName()
+                    "$monthName ${history.date.year}"
+                }
+            }
+        }
+    }
+
+    private fun Month.getDisplayName(): String {
+        return this.name.lowercase().replaceFirstChar { it.titlecase() }
+    }
+}
