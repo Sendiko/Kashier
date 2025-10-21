8e6d546b34e2bad6da3a9a1ed761f7d1b0a47992
Sendiko <sarangtawon897@gmail.com>
2025-10-21 11:13:51 +0700

    feat(di): integrate `createDataStore` into platform-specific and shared Koin modules
    - Added `createDataStore` to Android, iOS, and JVM `platformModules`.
    - Updated `sharedModules` to include `AppPreferences` and `UserLocalDataSource` bindings.

--- composeApp/src/commonMain/kotlin/org/chevalierlab/kashier/core/di/Modules.kt
+++ composeApp/src/commonMain/kotlin/org/chevalierlab/kashier/core/di/Modules.kt
@@ -1,8 +1,11 @@
 package org.chevalierlab.kashier.core.di
 
+import org.chevalierlab.kashier.core.preferences.AppPreferences
 import org.chevalierlab.kashier.home.data.HomeRepositoryImpl
 import org.chevalierlab.kashier.home.data.datasource.DummyDataSource
 import org.chevalierlab.kashier.home.data.datasource.DummyDataSourceImpl
+import org.chevalierlab.kashier.home.data.datasource.UserLocalDataSource
+import org.chevalierlab.kashier.home.data.datasource.UserLocalDataSourceImpl
 import org.chevalierlab.kashier.home.domain.repository.HomeRepository
 import org.chevalierlab.kashier.home.presentation.HomeViewModel
 import org.koin.core.module.Module
@@ -15,8 +18,11 @@ expect val platformModules: Module
 val sharedModules = module {
     /* Define modules here */
     /* From DataSourceImpl binded with interface -> RepositoryImpl binded with interface -> Factory ViewModel */
+
     singleOf(::DummyDataSourceImpl).bind<DummyDataSource>()
+    singleOf(::UserLocalDataSourceImpl).bind<UserLocalDataSource>()
     singleOf(::HomeRepositoryImpl).bind<HomeRepository>()
 
+    factory { AppPreferences(get()) }
     factory { HomeViewModel(get()) }
 }
