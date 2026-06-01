3e21dba633737bdc77eb27b55efeae55fb08688a
Sendiko <rizkysendiko7@gmail.com>
2026-06-01 17:28:48 +0700

    chore: cleanup imports

--- composeApp/src/commonMain/kotlin/org/chevalierlab/kashier/home/presentation/components/AddItemBottomSheet.kt
+++ composeApp/src/commonMain/kotlin/org/chevalierlab/kashier/home/presentation/components/AddItemBottomSheet.kt
@@ -1,18 +1,42 @@
 package org.chevalierlab.kashier.home.presentation.components
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kashier.composeapp.generated.resources.*
+import kashier.composeapp.generated.resources.Res
+import kashier.composeapp.generated.resources.add_item_title
+import kashier.composeapp.generated.resources.delete_button_label
+import kashier.composeapp.generated.resources.edit_item_title
+import kashier.composeapp.generated.resources.item_name_label
+import kashier.composeapp.generated.resources.item_name_placeholder
+import kashier.composeapp.generated.resources.item_price_label
+import kashier.composeapp.generated.resources.item_price_placeholder
+import kashier.composeapp.generated.resources.save_button_label
 import org.jetbrains.compose.resources.stringResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
