90abc0e648710b6f1cfa7e04c380819dbea76823
Sendiko <rizkysendiko7@gmail.com>
2026-06-01 14:01:49 +0700

    feat: load token from preferences

--- composeApp/src/commonMain/kotlin/org/chevalierlab/kashier/core/network/HttpClientFactory.kt
+++ composeApp/src/commonMain/kotlin/org/chevalierlab/kashier/core/network/HttpClientFactory.kt
@@ -3,15 +3,24 @@ package org.chevalierlab.kashier.core.network
 import io.ktor.client.*
 import io.ktor.client.engine.*
 import io.ktor.client.plugins.*
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.serialization.json.Json
+import org.chevalierlab.kashier.core.preferences.AppPreferences
 
 object HttpClientFactory {
-    fun create(engine: HttpClientEngine): HttpClient {
+    fun create(
+        engine: HttpClientEngine,
+        preferences: AppPreferences
+    ): HttpClient {
         return HttpClient(engine) {
             install(Logging) {
                 level = LogLevel.BODY
@@ -27,6 +36,22 @@ object HttpClientFactory {
                         ignoreUnknownKeys = true
                     }
                 )
+            }
+            install(Auth) {
+                bearer {
+                    loadTokens {
+                        val accessToken = preferences.getToken().firstOrNull()
+
+                        if (accessToken.isNullOrBlank()) {
+                            null
+                        } else {
+                            BearerTokens(
+                                accessToken = accessToken,
+                                refreshToken = ""
+                            )
+                        }
+                    }
+                }
             }
             install(HttpTimeout) {
                 requestTimeoutMillis = 5000
